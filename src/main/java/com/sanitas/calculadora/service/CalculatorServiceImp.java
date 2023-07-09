80a5080d1c5b504b326d689101893caa84c75925
meRodriguezA <merodriguezalbarran@gmail.com>
2023-07-09 23:46:28 +0200

    MERA: Service imp

--- src/main/java/com/sanitas/calculadora/service/CalculatorServiceImp.java
+++ src/main/java/com/sanitas/calculadora/service/CalculatorServiceImp.java
@@ -2,12 +2,23 @@ package com.sanitas.calculadora.service;
 
 import java.math.BigDecimal;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CalculatorServiceImp implements CalculatorService {
 
 	@Override
 	public BigDecimal calculate(BigDecimal firstParam, BigDecimal secondParam, String operator) {
-		// TODO Auto-generated method stub
-		return null;
+		switch (operator) {
+		case "suma": {
+			return firstParam.add(secondParam);
+		}
+		case "resta": {
+			return firstParam.subtract(secondParam);
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + operator);
+		}
 	}
 
 }
